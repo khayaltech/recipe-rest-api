35191b9394d32e0c5da626fbdb82c76e5bf5b82b
khayaltech <khayaltech@gmail.com>
2022-08-19 23:33:54 +0400

    Token Authentication Implemented

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,3 +11,25 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        email = attrs["email"]
+        password = attrs["password"]
+        user = authenticate(
+            request=self.context['request'],
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(msg, code='authorization')
+        attrs['user'] = user
+        return attrs
--- app/user/urls.py
+++ app/user/urls.py
@@ -5,4 +5,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.AuthTokenView.as_view(), name='token'),
+
 ]
