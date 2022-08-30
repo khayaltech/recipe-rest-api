54aa3f617a5afdf4ab6a930caf4302f9f69d10b9
khayaltech <khayaltech@gmail.com>
2022-08-31 01:54:17 +0400

    Ingredients and tags assigned to any recipe tag implemented

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -93,6 +93,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes.',
+            ),
+        ]
+    )
+)
 class BaseAttrRecipeViewset(mixins.ListModelMixin,
                             mixins.UpdateModelMixin,
                             mixins.DestroyModelMixin, viewsets.GenericViewSet):
@@ -102,8 +113,17 @@ class BaseAttrRecipeViewset(mixins.ListModelMixin,
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """Filtering queryset to authenticted user"""
-        return self.queryset.filter(user=self.request.user.id).order_by('-name')
+        """Filter queryset to authenticated user."""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
 
 class TagViewSet(BaseAttrRecipeViewset):
